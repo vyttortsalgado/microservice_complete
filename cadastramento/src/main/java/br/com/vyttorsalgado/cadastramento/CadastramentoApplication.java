a7a02affb07a87eaf38d49c925b8565ce38ef917
Vyttor Salgado <vyttortsalgado@hotmail.com>
2020-07-26 00:08:06 -0300

    Primeiro commit

---
+++ cadastramento/src/main/java/br/com/vyttorsalgado/cadastramento/CadastramentoApplication.java
@@ -0,0 +1,13 @@
+package br.com.vyttorsalgado.cadastramento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastramentoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CadastramentoApplication.class, args);
+    }
+
+}
